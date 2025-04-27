6361cbe8e76b6d69088f5892925dcf6ace48ae00
insafim <i.m.insaf@gmail.com>
2025-04-28 02:38:33 +0400

    changed env name

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 
+ENV_NAME=.molmo-train
+conda create -n $ENV_NAME python=3.11 -y
+conda activate $ENV_NAME
 git clone https://github.com/hiyouga/LLaMA-Factory
 cd LLaMA-Factory
 pip3 install -e ".[torch,deepspeed,vllm,bitsandbytes,metrics,liger-kernel]"
